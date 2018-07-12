be0877230ead647635d8cc247c29794b6c07fb44
Manuel Ruiz <manuel.ruiz@rte-france.com>
2018-07-12 15:52:44 +0200

    new comparator

--- src/algebra/order.jl
+++ src/algebra/order.jl
@@ -25,6 +25,7 @@ end
   BEWARE: Order is valid for fully real or conjugated exponents. Mixed case is not treated.
 """
 function isless(exp1::Exponent, exp2::Exponent)
+    # return isless(exp1.expo, exp2.expo)
   exp1_explsum, exp1_conjsum = get_sumdegs(exp1)
   exp2_explsum, exp2_conjsum = get_sumdegs(exp2)
   # exp1_explsum==0 || exp1_conjsum==0 || warn("isless(::Exponent, Exponent): exp1 has expl and conj vars, order may be ill defined...") # TODO
@@ -34,23 +35,41 @@ function isless(exp1::Exponent, exp2::Exponent)
   if exp1_deg < exp2_deg
     return true
   elseif exp1_deg == exp2_deg
-    vars = SortedSet(keys(exp1))
-    union!(vars, keys(exp2))
-    for var in vars
-      if !haskey(exp2, var) # hence haskey(exp1, var)
-        return true
-      elseif !haskey(exp1, var) # hence haskey(exp2, var)
-        return false
-      elseif exp1[var] > exp2[var]
-        return true
-      elseif exp1[var] == exp2[var]
-        continue
-      else
-        return false
-      end
+    state1 = start(exp1)
+    state2 = start(exp2)
+    while !done(exp1, state1) && !done(exp2, state2)
+        (i1, state1) = next(exp1, state1)
+        (i2, state2) = next(exp2, state2)
+        if isequal(i1, i2)
+            continue
+        else
+            return isless(i1,i2)
     end
-  else
-    return false
+    if done(exp1, state1) && done(exp2, state2)
+        return false
+    elseif done(exp1, state1) && !done(exp2, state2)
+        return true
+    elseif !done(exp1, state1) && done(exp2, state2)
+        return false
+    end
+end
+  #   vars = SortedSet(keys(exp1))
+  #   union!(vars, keys(exp2))
+  #   for var in vars
+  #     if !haskey(exp2, var) # hence haskey(exp1, var)
+  #       return true
+  #     elseif !haskey(exp1, var) # hence haskey(exp2, var)
+  #       return false
+  #     elseif exp1[var] > exp2[var]
+  #       return true
+  #     elseif exp1[var] == exp2[var]
+  #       continue
+  #     else
+  #       return false
+  #     end
+  #   end
+  # else
+  #   return false
   end
   return false
 end
